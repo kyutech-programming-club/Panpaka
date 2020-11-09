3c2d6af35c6f58866d1cf4dd95bd449b17190acf
shion200 <shion1390@icloud.com>
2020-11-09 18:44:45 +0900

    Moving sphere

---
+++ Assets/Scripts/DropPlace.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropPlace : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData data){
+        Debug.Log(gameObject.name);
+
+        CardMovement dragObj = data.pointerDrag.GetComponent<CardMovement>();
+        if(dragObj != null){
+            dragObj.parentTransform = this.transform;
+            Debug.Log(gameObject.name+"に"+data.pointerDrag.name+"をドロップ");
+        }
+
+    }
+}
---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class move : MonoBehaviour, IDropHandler
+{   
+    public void OnDrop(PointerEventData data){
+        Debug.Log(gameObject.name);
+
+        CardMovement dragObj = data.pointerDrag.GetComponent<CardMovement>();
+        if(dragObj != null){
+            dragObj.parentTransform = this.transform;
+            Debug.Log(gameObject.name+"に"+data.pointerDrag.name+"をドロップ");
+            if(dragObj.CompareTag("flower"))
+                {
+                    gameObject.transform.Translate(1,1,1);
+                }
+        }
+    }
+
+}
