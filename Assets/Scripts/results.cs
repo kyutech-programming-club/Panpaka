590f46bda891244bc6623b31b73dd290bb03a1ba
shion200 <shion1390@icloud.com>
2020-11-10 18:02:23 +0900

    Create stage

---
+++ Assets/Scripts/results.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class results : MonoBehaviour
+{
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("results");
+    }
+}
