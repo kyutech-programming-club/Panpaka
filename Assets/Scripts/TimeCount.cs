3a1946c5524eb83598c6d6642e55cd3775c7a603
shion200 <shion1390@icloud.com>
2020-11-21 12:02:19 +0900

    Fixed timecount script

--- Assets/Scripts/TimeCount.cs
+++ Assets/Scripts/TimeCount.cs
@@ -8,40 +8,47 @@ public class TimeCount : MonoBehaviour {
     //時間を記録する小数も入る変数.
     Text text;
     public GameObject C;
-    public static float time = 0f;
+    public static float time;
     
     void Start () {
         text = GetComponent<Text>();//自分のインスペクター内からTextコンポーネントを取得.
+        time = 0;
     }
 	
-    void Update () {
-        time += Time.deltaTime;//毎フレームの時間を加算.
-        int minute = (int)time/60;//分.timeを60で割った値.
-        int second = (int)time%60;//秒.timeを60で割った余り.
-        int msecond = (int)(time*100%100);
-        string minText, secText, msecText;//テキスト形式の分・秒を用意.
+    void Update () {      
+            if(C.transform.position.x >= 2)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                time += Time.deltaTime;//毎フレームの時間を加算.
+                int minute = (int)time/60;//分.timeを60で割った値.
+                int second = (int)time%60;//秒.timeを60で割った余り.
+                int msecond = (int)(time*100%100);
+                string minText, secText, msecText;//テキスト形式の分・秒を用意.
         
-        if (minute < 10)
-            minText = "0" + minute.ToString();//("0"埋め), ToStringでint→stringに変換.
-        else
-            minText = minute.ToString();
+                if (minute < 10)
+                    minText = "0" + minute.ToString();//("0"埋め), ToStringでint→stringに変換.
+                else
+                    minText = minute.ToString();
         
-	    if (second < 10)
-            secText = "0" + second.ToString();//上に同じく.
-        else
-            secText = second.ToString();
+	            if (second < 10)
+                    secText = "0" + second.ToString();//上に同じく.
+                else
+                    secText = second.ToString();
 
-        if(msecond < 10)
-            msecText = "0" + msecond.ToString ();
-        else
-            msecText = msecond.ToString ();
+                if(msecond < 10)
+                    msecText = "0" + msecond.ToString ();
+                else
+                    msecText = msecond.ToString ();
 
-        text.text = "[Time] " + minText + ":" + secText + "." + msecText ;
-
-        if(C.transform.position.x >= 2)
-        {
-            Time.timeScale = 0;
-        }
+                text.text = "[Time] " + minText + ":" + secText + "." + msecText ;
+            } 
+            
+        
+        
     }
 
     public static float getTime()
