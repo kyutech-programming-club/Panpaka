18d3dd80c81a7788c163278c4cabdf151a080dbc
shion200 <shion1390@icloud.com>
2020-11-25 22:15:48 +0900

    Plus UnityChann movement

--- Assets/Scripts/DropPlace.cs
+++ Assets/Scripts/DropPlace.cs
@@ -6,7 +6,7 @@ public class DropPlace : MonoBehaviour, IDropHandler
     public GameObject C;
     public GameObject childObject;
     public Transform parent;
-    // public GameObject Controller;
+
     public void OnDrop(PointerEventData data){
         Debug.Log(gameObject.name);
 
@@ -16,27 +16,28 @@ public class DropPlace : MonoBehaviour, IDropHandler
             Debug.Log(gameObject.name+"に"+data.pointerDrag.name+"をドロップ");
             if(dragObj.CompareTag("right"))
                 {
-                     C.transform.Translate(1,0,0);
+                    C.GetComponent<UnityChanAttempt>().RightRun();
+                    //  C.transform.Translate(1,0,0);
                 }
             if(dragObj.CompareTag("left"))
                 {
                     if(C.transform.position.x <= -8)
                     {
                         Instantiate (childObject, new Vector3(400,250,0), Quaternion.identity,parent);
-
-
-                        
                     }
                     else
                     {
-                        C.transform.Translate(-1,0,0);
+                        C.GetComponent<UnityChanAttempt>().LeftRun();
+                        // C.transform.Translate(-1,0,0);
+                        
                     }
 
 
                 }
             if(dragObj.CompareTag("up"))
                 {
-                     C.transform.Translate(0,1,0);
+                    C.GetComponent<UnityChanAttempt>().Jump();
+                    //  C.transform.Translate(0,1,0);
                 }
             if(dragObj.CompareTag("down"))
                 {
