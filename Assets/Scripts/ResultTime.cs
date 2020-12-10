6ded845802c361f0b486e67409f50f6f4a2870ab
tetsuomi <kondo.tetta851@mail.kyutech.jp>
2020-12-10 21:03:08 +0900

    Chage the how to calc score in stage 1 2 3

--- Assets/Scripts/ResultTime.cs
+++ Assets/Scripts/ResultTime.cs
@@ -7,41 +7,47 @@ using NCMB;
 public class ResultTime : MonoBehaviour
 {
     float time;
+    int commandCount;
     Text text;
-    // Start is called before the first frame update
+
     void Start()
     {
         text = GetComponent<Text>();
         time = NewBehaviourScript.getTime();
-        int minute = (int)time/60;//分.timeを60で割った値.
-        int second = (int)time%60;//秒.timeを60で割った余り.
-        int msecond = (int)(time*100%100);
+        commandCount = CommandCount.count;
+
+
+        text.text = "[時間] " + ChangeTimeToString(time);
+        text.text += "\n[コマンド回数] " + commandCount.ToString();
+        text.text += "\n================";
+        text.text += "\n[点数] " + ChangeTimeToString(time + (float)(commandCount*5));
+
+        //SaveResult(time);
+    }
+
+    string ChangeTimeToString(float time)
+    {
+        int minute = (int)time / 60;//分.timeを60で割った値.
+        int second = (int)time % 60;//秒.timeを60で割った余り.
+        int msecond = (int)(time * 100 % 100);
         string minText, secText, msecText;//テキスト形式の分・秒を用意.
-        
+
         if (minute < 10)
             minText = "0" + minute.ToString();//("0"埋め), ToStringでint→stringに変換.
         else
             minText = minute.ToString();
-        
-	    if (second < 10)
+
+        if (second < 10)
             secText = "0" + second.ToString();//上に同じく.
         else
             secText = second.ToString();
 
-        if(msecond < 10)
-            msecText = "0" + msecond.ToString ();
+        if (msecond < 10)
+            msecText = "0" + msecond.ToString();
         else
-            msecText = msecond.ToString ();
+            msecText = msecond.ToString();
 
-        text.text = "[Time] " + minText + ":" + secText + "." + msecText;
-
-        SaveResult(time);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        return minText + ":" + secText + "." + msecText;
     }
 
     void SaveResult(float time)
